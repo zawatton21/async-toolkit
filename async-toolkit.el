f1166a1d767d8b6b8f65e69509d9243e7b7bbe7d
zawatton21 <kurozawawo@gmail.com>
2024-04-28 16:06:55 +0900

    2024-04-28

--- async-toolkit.el
+++ async-toolkit.el
@@ -47,63 +47,14 @@
        (file-exists-p (concat buffer-file-name ".#"))))
 
 ;;;###autoload
-(defun async-external-save-buffer ()
-  "Save the current buffer using an external process asynchronously and measure elapsed time for debugging."
-  (interactive)
-  (require 'cl-lib)
-  (require 'async)
-
-  (unless (buffer-file-name)
-    (message "Buffer is not associated with a file. Skipping external save.")
-    (cl-return-from async-external-save-buffer))
-
-  (let* ((start-time (current-time))
-         (file-name (buffer-file-name))
-         (buf-content (buffer-string))
-         (temp-file (make-temp-file "emacs-external-save" nil ".tmp" buf-content)))
-
-    (let* ((win-temp-file (replace-regexp-in-string "/" "\\\\" temp-file))
-           (win-file-name (replace-regexp-in-string "/" "\\\\" file-name))
-           (cmd (if (eq system-type 'windows-nt)
-                    (format "move /Y \"%s\" \"%s\"" (shell-quote-argument win-temp-file) (shell-quote-argument win-file-name))
-                  (format "mv %s %s" (shell-quote-argument temp-file) (shell-quote-argument file-name)))))
-
-      ;; Convert file paths for Windows, if necessary
-      (when (eq system-type 'windows-nt)
-        (setq win-temp-file (encode-coding-string win-temp-file 'utf-8)
-              win-file-name (encode-coding-string win-file-name 'utf-8)))
-
-      ;; Use async-start to run the shell command asynchronously
-      (async-start
-       `(lambda ()
-          (shell-command ,cmd))
-       `(lambda (result)
-          (if (= result 0)
-              (progn
-                (message "Buffer saved using external process asynchronously.")
-                (with-current-buffer ,(current-buffer)
-                  ;; Update the buffer's file modification time to the latest
-                  (let ((new-modtime (nth 5 (file-attributes ,file-name))))
-                    (set-visited-file-modtime new-modtime))
-                  (revert-buffer t t t)
-                  (set-buffer-modified-p nil)
-                  (run-hooks 'after-save-hook))
-                ;; Clean up the temporary file
-                (delete-file ,temp-file))
-            ;; If the operation failed, display the output buffer for diagnostics
-            (with-current-buffer (get-buffer-create "*External Save Output*")
-              (insert "Error during external save: " (number-to-string result))
-              (display-buffer (current-buffer)))))))))
-
-;;;###autoload
 (defun async-save-buffer-internal (orig-func &rest args)
   "Advice to optionally redirect `save-buffer` based on `enable-async-save-buffer-advice`."
-  (if enable-async-save-buffer-advice
-      (if async-save-in-progress
-          (apply orig-func args)  ; async-save-buffer からの再帰呼び出しを避ける
-        (async-external-save-buffer-with-update))  ; 通常の save-buffer 呼び出しを async-save-buffer にリダイレクト
-    (apply orig-func args)))  ; 制御用変数が無効な場合は、常に元の関数を実行
-
+  (if (not enable-async-save-buffer-advice)
+      (apply orig-func args)  ; Always run the original function if the control variable is disabled
+    (if async-save-in-progress
+        (apply orig-func args)  ; Avoid recursive call if async save is already in progress
+      (let ((async-save-in-progress t))
+        (async-external-save-buffer)))))  ; Redirect to async version
 
 (advice-add 'save-buffer :around #'async-save-buffer-internal)
 
@@ -132,7 +83,7 @@ Uses `enable-async-save-some-buffers-advice` to determine behavior."
                 (with-current-buffer buffer
                   (when (and buffer-save-without-query (buffer-modified-p))
                     (push (buffer-name) autosaved-buffers)
-                    (async-external-save-buffer-with-update))))
+                    (async-external-save-buffer))))
               (setq files-done
                     (map-y-or-n-p
                      (lambda (buffer)
@@ -144,7 +95,7 @@ Uses `enable-async-save-some-buffers-advice` to determine behavior."
                            (format "Asynchronously save buffer %s? " (buffer-name buffer)))))
                      (lambda (buffer)
                        (with-current-buffer buffer
-                         (async-external-save-buffer-with-update)))
+                         (async-external-save-buffer)))
                      (files--buffers-needing-to-be-saved pred)
                      '("buffer" "buffers" "save")
                      save-some-buffers-action-alist))
@@ -221,10 +172,15 @@ Uses `enable-async-save-some-buffers-advice` to determine behavior."
 (defvar enable-async-revert-advice nil
   "Control whether `async-revert-buffer' should be used.")
 
+;;; グローバル変数の定義
+(defvar async-diff-command "diff --speed-large-files"
+  "The command used to compare files in `my-async-replace-buffer-contents-with`.")
+
 ;;;###autoload
 (defun async-replace-buffer-contents-with (temp-buffer-name)
   "Replace the current buffer's contents with that of TEMP-BUFFER-NAME asynchronously, if there are differences."
   (let* ((target-buffer (current-buffer))
+         (file-name (buffer-file-name target-buffer)) ; ファイル名を変数に保存
          (temp-buffer (get-buffer temp-buffer-name)))
     (unless temp-buffer
       (error "Temporary buffer does not exist"))
@@ -232,56 +188,39 @@ Uses `enable-async-save-some-buffers-advice` to determine behavior."
                       (make-temp-file "emacs-async-replace" nil ".tmp"))))
       ;; 一時ファイルに内容を書き込む
       (write-region (point-min) (point-max) temp-file nil 'silent)
-      (let ((diff-cmd (format "diff --speed-large-files %s %s" (shell-quote-argument temp-file) (shell-quote-argument (buffer-file-name target-buffer)))))
+      (let ((diff-cmd (format "%s %s %s" 
+                              async-diff-command
+                              (shell-quote-argument temp-file) 
+                              (shell-quote-argument file-name))))
         ;; 非同期diffコマンドを開始
-        (let ((start-time (current-time)))
-          (async-start
-           `(lambda ()
-              (let ((diff-start-time (current-time)))
-                ;; diffコマンドの実行と、開始時刻とのペアを返す
-                (cons (shell-command-to-string ,diff-cmd) diff-start-time)))
-           `(lambda (result)
-              ;; diffの結果と開始時刻を分解
-              (let* ((diff-output (car result))
-                     (diff-start-time (cdr result))
-                     (end-time (current-time)))
-                (when (buffer-live-p ,target-buffer)
-                  (if (string-empty-p diff-output)
-                      nil  ;; 差分がない場合は何もしない
-                    (with-current-buffer ,target-buffer
-                      (erase-buffer)
-                      (insert-file-contents ,temp-file)
-                      ;; ファイルの最新の修正時刻を使用してバッファの変更時刻を更新
-                      (let ((new-modtime (nth 5 (file-attributes (buffer-file-name)))))
-                        (set-visited-file-modtime new-modtime)))))
-                  (delete-file ,temp-file)))))))))
+        (async-start
+         `(lambda ()
+            (shell-command-to-string ,diff-cmd))
+         `(lambda (diff-output)
+            (when (buffer-live-p ,target-buffer)
+              (if (string-empty-p diff-output)
+                  (message "No changes detected.")
+                (progn
+                  ;; 差分がある場合のみバッファを更新
+                  (with-current-buffer ,target-buffer
+                    (erase-buffer)
+                    (insert-file-contents ,temp-file)
+                    (let ((new-modtime (nth 5 (file-attributes ,file-name))))
+                      (set-visited-file-modtime new-modtime)))
+                  (message "Buffer updated asynchronously.")))
+              (delete-file ,temp-file))))))))
 
 ;;;###autoload
 (defun async-revert-buffer ()
   "Asynchronously revert the current buffer using an external process, keeping as much buffer state as possible."
   (interactive)
-  (let ((start-time (current-time))
-        (file-name (buffer-file-name)))
+  (let ((file-name (buffer-file-name)))
     (unless file-name
       (error "Buffer is not visiting a file"))
-    ;; 外部コマンドの代わりに、一時バッファを用いる
-    (let ((output (with-temp-buffer
-                    (insert-file-contents file-name)
-                    (buffer-string)))
-          (read-end-time (current-time)))
-      ;;(message "Time to read file into temp buffer: %fs" 
-      ;;          (float-time (time-subtract read-end-time start-time)))
-      ;; 現在のバッファと一時バッファの内容を置き換え
-      (let ((temp-buffer (generate-new-buffer " *temp file content*")))
-        (with-current-buffer temp-buffer
-          (insert output))
-        (async-replace-buffer-contents-with temp-buffer)
-        ;;(replace-buffer-contents temp-buffer)
-        (kill-buffer temp-buffer)
-        (let ((end-time (current-time)))
-          ;;(message "Total async revert time: %fs" 
-          ;;         (float-time (time-subtract end-time start-time)))
-          )))))
+    (let ((temp-buffer-name (generate-new-buffer " *temp file content*")))
+      (with-current-buffer temp-buffer-name
+        (insert-file-contents file-name))
+      (async-replace-buffer-contents-with temp-buffer-name))))
 
 ;;;###autoload
 (defun async-revert-advice (original-revert &rest args)
